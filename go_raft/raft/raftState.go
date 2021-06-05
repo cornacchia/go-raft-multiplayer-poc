f481b7a4fbee1e7f71215b07c8a10b8aa69447f6
cornacchia <pozzan.gabriele@gmail.com>
2021-06-05 12:31:23 +0200

    Change quorum function

--- go_raft/raft/raftState.go
+++ go_raft/raft/raftState.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"sync"
 	"time"
+	"math"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -385,6 +386,10 @@ func (_state *stateImpl) getElectionTimer() *time.Timer {
 	return _state.electionTimer
 }
 
+func (_state *stateImpl) getQuorum() int {
+	return int(math.Ceil(float64(2 * len(_state.serverConfiguration) + 1) / 3.0))
+}
+
 func (_state *stateImpl) updateElection(resp *RequestVoteResponse) bool {
 	_state.lock.Lock()
 	var hashed = getRequestVoteResponseBytes(resp)
@@ -399,8 +404,9 @@ func (_state *stateImpl) updateElection(resp *RequestVoteResponse) bool {
 		_state.currentVotes[(*resp).Id] = *resp
 		_state.currentElectionVotes++
 
-		log.Debug("Election votes: ", _state.currentElectionVotes, "/", len(_state.serverConfiguration)/2)
-		if _state.currentElectionVotes >= len(_state.serverConfiguration)/2 {
+		log.Debug("Election votes: ", _state.currentElectionVotes, "/", _state.getQuorum())
+		// TEST: repeat tests with
+		if _state.currentElectionVotes >= _state.getQuorum() {
 			_state.lock.Unlock()
 			_state.winElection()
 			return true
@@ -645,8 +651,8 @@ func (_state *stateImpl) verifyAppendEntriesQuorum(aea *AppendEntriesArgs) bool 
 		}
 	}
 
-	log.Info("Verify append entries quorum (", (*aea).LeaderID, "): ", votes, "/", len(_state.serverConfiguration)/2)
-	return votes >= len(_state.serverConfiguration)/2
+	log.Info("Verify append entries quorum (", (*aea).LeaderID, "): ", votes, "/", _state.getQuorum())
+	return votes >= _state.getQuorum()
 }
 
 func (_state *stateImpl) updateQuorum(sid ServerID, lastLogIdx int, lastLogHash [32]byte) {
@@ -925,7 +931,7 @@ func (_state *stateImpl) checkCommits() {
 				replicatedFollowers++
 			}
 		}
-		if replicatedFollowers > len(_state.serverConfiguration)/2 {
+		if replicatedFollowers > _state.getQuorum() {
 			if _state.logs[i].Term == _state.currentTerm {
 				_state.commitIndex = _state.logs[i].Idx
 			}
